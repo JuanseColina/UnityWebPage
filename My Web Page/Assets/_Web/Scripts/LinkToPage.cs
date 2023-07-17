307167e217a501099d7a201e092833f26a55682e
Juan Sebastian Colina Franco <juansebas_colina@hotmail.com>
2023-07-17 16:49:08 -0400

    Adding bubbles

--- My Web Page/Assets/_Web/Scripts/LinkToPage.cs
+++ My Web Page/Assets/_Web/Scripts/LinkToPage.cs
@@ -7,13 +7,15 @@ public class LinkToPage : MonoBehaviour
     public void LinkToPageMethod(string url)
     {
         Application.OpenURL(url);
-        // an option to mail to me
     }
 
     [SerializeField] [CanBeNull] private GameObject backGroundWhite;
     
     public void BackGroundWhite(bool isEnter)
     {
-        if (backGroundWhite != null) backGroundWhite.SetActive(isEnter);
+        if (backGroundWhite != null)
+        {
+            backGroundWhite.SetActive(isEnter);
+        }
     }
 }
